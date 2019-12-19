4e926ba845a381896481db1da868510c6e9714fc
yeahchino <diegofeitt@gmail.com>
2019-12-19 18:03:42 -0300

    v1.0 Front-End Dev Challenge

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   /******** Homepage Carousel - All Photos *********/
 
   // Build API Request
-  var olapicEndpoint = "";
+  var olapicEndpoint = "https://photorankapi-a.akamaihd.net/customers/215757/media/shuffled?rights_given=0&include_tagged_galleries=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
+  //endpoint tested with postman "GET MEDIA OF A STREAM"
+  
 
   $.ajax({
     dataType: "json",
@@ -15,10 +17,12 @@ $(document).ready(function() {
     success: function(data) {     
       
       // Create array of photos pulled from the Olapic API response
-      var mediaArray = ;
+      var mediaArray = data.data._embedded.media;
 
       // Loop through the mediaArray
-      
+      for (x=0; x < mediaArray.length; x++) {
+        var mediaItem = mediaArray[x];
+        var olapicImage = mediaItem.images.mobile;
 
       // Append the images to the carousel
         $(".owl-carousel").append("<div class='item olapic-image'><img src='" + olapicImage + "'></div>");
@@ -29,13 +33,13 @@ $(document).ready(function() {
         items: 4,
         loop: true,
         touchDrag: true,
-        nav: false,
+        nav: true, //set true in order to navigate
         navText: ["<i class='arrow left'></i>", "<i class='arrow right'></i>"],
         dots: false,
         video: true,
         slideBy: 'page',
         navSpeed: 150,
-        autoplay: false,
+        autoplay: true, // set true for autoplay
         autoplayTimeout: 2000,
         autoplaySpeed: 150,
         responsive: {
